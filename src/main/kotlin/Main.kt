6e3455195b7d67693a6a4a419db3a1a4822a1325
GerasimovK <k.gerasimov@cifrium.ru>
2022-08-03 17:53:15 +0300

    Сделано без учёта условия про доп 1% и хранения информации о "меломанах"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,23 @@
 package ru.cifrium
 
 fun main () {
-    val itemSum1: Double = 3000
+    val itemSum1: Int = 3000_00
     val itemDiscount: Int = 100_00
     val itemMaxdiscount: Double = 0.05
     val itemExtradiscount: Double = 0.01
     var FinalPrice: Double
 
     if (itemSum1 <= 1000_00) {
-        itemSum1 = FinalPrice  ()
+        FinalPrice = itemSum1 ()
     }
-    var TotalDiscount: =
+    if (itemSum1 >= 1000_00) but (itemSum1 <= 10_000_00) {
+        FinalPrice = itemSum1 - itemDiscount ()
+    }
+    if (itemSum1 > 10_001_00) {
+        FinalPrice = itemSum1 - (itemSum1 * itemMaxdiscount) {
+        }
+    }
 
-
-    println("Hello World!")
+    println("Итого к оплате $FinalPrice")
 
 }
