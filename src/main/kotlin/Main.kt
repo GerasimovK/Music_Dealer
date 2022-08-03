15f69da3654e2f30569ac6ffd9a1c03c68c57b9e
Кирилл Герасимов <kirillgerasimov@MacBook-Pro-Kirill.local>
2022-08-03 17:29:06 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+package ru.cifrium
+
+fun main () {
+    val itemSum1: Double = 3000
+    val itemDiscount: Int = 100_00
+    val itemMaxdiscount: Double = 0.05
+    val itemExtradiscount: Double = 0.01
+    var FinalPrice: Double
+
+    if (itemSum1 <= 1000_00) {
+        itemSum1 = FinalPrice  ()
+    }
+    var TotalDiscount: =
+
+
+    println("Hello World!")
+
+}
