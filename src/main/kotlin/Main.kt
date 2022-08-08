892fe9d09c38fb23784ce8ee65bd9eb286109165
GerasimovK <k.gerasimov@cifrium.ru>
2022-08-09 00:10:26 +0300

    Работающий код после рефакторинга (финал)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,32 +5,28 @@ fun main () {
     var itemCurrentSum: Int = 1000_00
 
     //  сумма предыдущих покупок
-    val itemSum1: Int = 100001_00
+    val itemSum1: Int = 30000_00
 
     // если пользователь покупает каждый месяц - ставим true, если нет - то false
-    var history: Boolean = false
+    var history: Boolean = true
 
     val itemDiscount: Int = 100_00
     val itemMaxDiscount: Double = 0.05
     val itemExtradiscount: Double = 0.01
     var finalPrice: Int = itemCurrentSum
+    var itemCurrentDiscountSum: Int = 0
 
-    if ( itemSum1 >= 0 && itemSum1 <= 1000_00 && history == true){
-        finalPrice = (itemCurrentSum - (itemCurrentSum * itemExtradiscount)).toInt()
+
+    if (itemSum1 >= 1001_00 && itemSum1 <= 10_000_00){
+        itemCurrentDiscountSum = itemDiscount
     }
-    if (itemSum1 >= 1001_00 && itemSum1 <= 10_000_00 && history == true) {
-        finalPrice = ((itemCurrentSum - itemDiscount)).toInt()
+    if (itemSum1 > 10_001_00) {
+        itemCurrentDiscountSum = (finalPrice * itemMaxDiscount).toInt()
+    }
+    finalPrice = finalPrice - itemCurrentDiscountSum
+
+    if (history == true){
         finalPrice = finalPrice - (finalPrice * itemExtradiscount).toInt()
-    }
-    if (itemSum1 >= 1001_00 && itemSum1 <= 10_000_00 && history == false) {
-        finalPrice = ((itemCurrentSum - itemDiscount)).toInt()
-    }
-    if (itemSum1 > 10_001_00 && history == true) {
-        finalPrice = (itemCurrentSum - (itemCurrentSum * itemMaxDiscount)).toInt()
-        finalPrice = finalPrice - (finalPrice * itemExtradiscount).toInt()
-    }
-    if (itemSum1 > 10_001_00 && history == false) {
-        finalPrice = (itemCurrentSum - (itemCurrentSum * itemMaxDiscount)).toInt()
     }
     println("Your final price is $finalPrice")
 }
