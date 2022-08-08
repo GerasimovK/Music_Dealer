7d508bd7c5079c2511b139aaf4effcfd71cb7c8e
GerasimovK <k.gerasimov@cifrium.ru>
2022-08-08 23:36:42 +0300

    Работающий код до рефакторинга

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,36 @@
 package ru.cifrium
 
 fun main () {
-    val itemSum1: Double = 3000_00.00
+    // сумма текущей покупки
+    var itemCurrentSum: Int = 1000_00
+
+    //  сумма предыдущих покупок
+    val itemSum1: Int = 3000_00
+
+    // если пользователь покупает каждый месяц - ставим true, если нет - то false
+    var history: Boolean = true
+
     val itemDiscount: Int = 100_00
-    val itemMaxdiscount: Double = 0.05
+    val itemMaxDiscount: Double = 0.05
     val itemExtradiscount: Double = 0.01
-    var finalPrice: Double
+    var finalPrice: Int = itemCurrentSum
 
-    if (itemSum1 <= 1000_00) {
-        finalPrice = itemSum1.toDouble()
+    if ( itemSum1 > 1 && itemSum1 <= 1000 && history == true){
+        finalPrice = (itemCurrentSum - (itemCurrentSum * itemExtradiscount)).toInt()
     }
-    if (itemSum1 >= 1000_00 && itemSum1 <= 10_000_00) {
-        finalPrice = (itemSum1 - itemDiscount).toDouble()
+    if (itemSum1 >= 1001_00 && itemSum1 <= 10_000_00 && history == true) {
+        finalPrice = ((itemCurrentSum - itemDiscount)).toInt()
+        finalPrice = finalPrice - (finalPrice * itemExtradiscount).toInt()
     }
-    if (itemSum1 > 10_001_00) {
-        finalPrice = itemSum1 - (itemSum1 * itemMaxdiscount) {
-        }
+    if (itemSum1 >= 1001_00 && itemSum1 <= 10_000_00 && history == false) {
+        finalPrice = ((itemCurrentSum - itemDiscount)).toInt()
     }
-
-    println("Итого к оплате $finalPrice")
-    // Проверка изменений 3
-
+    if (itemSum1 > 10_001_00 && history == true) {
+        finalPrice = (itemCurrentSum - (itemCurrentSum * itemMaxDiscount)).toInt()
+        finalPrice = finalPrice - (finalPrice * itemExtradiscount).toInt()
+    }
+    if (itemSum1 > 10_001_00 && history == false) {
+        finalPrice = (itemCurrentSum - (itemCurrentSum * itemMaxDiscount)).toInt()
+    }
+    println("Your final price is $finalPrice")
 }
